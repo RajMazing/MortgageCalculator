3707ac6a9c65f60ceb93bb1c94deb897fe94f1a1
RajMazing <caseyraj92@gmail.com>
2022-09-01 20:11:45 -0500

    Initial commit

---
+++ src/com/firstproject/Main.java
@@ -0,0 +1,21 @@
+package com.firstproject;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        Final byte PERCENT = 100;
+        Scanner scanner =  new Scanner(System.in);
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.println("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.println("Period (Years): ");
+
+
+    }
+}
